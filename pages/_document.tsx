2fa229bb3aff5fb13742ce68629252d1236d38c4
Yiannis Hadjiyianni <hadjiyiannis.3@gmail.com>
2026-03-26 04:33:48 +0100

    fix: mino rbug

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <title>SISYPHUS</title>
         <link rel="icon" href="/favicon.ico" />
         <link
           rel="preload"
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useRef, useState } from "react";
 import localFont from "next/font/local";
 
 import Sidebar from "./components/Sidebar";
@@ -16,64 +15,12 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
-const songListLength = 3;
-
 export default function Home() {
-  const [currentSong, setCurrentSong] = useState<number>(0);
-  const [paused, setPaused] = useState<boolean>(true);
-  const audioRef = useRef(null);
-
-  useEffect(() => {
-    if (audioRef.current) {
-      // @ts-expect-error -- IGNORE
-      audioRef.current.onended = () => {
-        setCurrentSong((currentSong + 1) % songListLength);
-        // @ts-expect-error -- IGNORE
-        audioRef.current.src = `/assets/audio/${currentSong}.mp3`;
-        // @ts-expect-error -- IGNORE
-        audioRef.current.play();
-      }
-      
-      // @ts-expect-error -- IGNORE
-      audioRef.current.play();
-    }
-  }, []);
-
-  // Function to play the audio
-  const handlePlay = () => {
-    if (audioRef.current) {
-      // @ts-expect-error -- IGNORE
-      audioRef.current.play();
-      setPaused(false);
-    }
-  };
-
-  // Function to pause the audio
-  const handleStop = () => {
-    if (audioRef.current) {
-      // @ts-expect-error -- IGNORE
-      audioRef.current.pause();
-      // audioRef.current.currentTime = 0; // Reset the audio to the beginning
-      setPaused(true);
-    }
-  };
-
   return (
     <div
       className={`${geistSans.variable} ${geistMono.variable} items-center justify-items-center min-h-screen p-2 sm:p-4 md:p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
-      {/* <audio ref={audioRef} src="/assets/audio/1.mp3" autoPlay /> */}
       <main className="relative flex flex-col lg:flex-row items-start sm:items-start border border-green-500">
-        {/* {
-          paused ?
-          <button
-          className="absolute top-0 right-0 text-violet-500 font-bold bg-white"
-          onClick={handlePlay}>🎵Play</button>
-          :
-          <button
-          className="absolute top-0 right-0 text-violet-500 font-bold bg-white"
-          onClick={handleStop}>🎵Stop</button>
-        } */}
         {/* Navigation links: above the card on mobile, to the right of the card on desktop */}
         <div className="order-1 lg:order-2 w-full lg:w-auto px-2 pt-4 lg:pt-8 lg:pl-2 flex lg:block justify-center">
           <div className="flex flex-col items-center gap-2 lg:items-start">
